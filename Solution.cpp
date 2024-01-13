4acc4c72a531b9cf34ad5e5f25aa7e6e9ac9f633
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-01-13 22:30:48 +0100

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 /*
 Interface BinaryMatrix is provided by leetcode.com
+Do not include the interface when running the code on the website.
 The comments below are from leetcode.com
  */
 // This is the BinaryMatrix's API interface.
